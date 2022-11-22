8b623a63997ac0cee3f53bc86538c7bc9a60bbfe
Tobija Žuntar <tobija.zuntar@gmail.com>
2022-11-22 08:46:36 +0100

    Move alert and notice to the application helper, add display_username to avoid errors when the user isn't signed in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def display_notice
+    content_tag(:p, notice, class: 'notice') if notice.present?
+  end
+
+  def display_alert
+    content_tag(:p, alert, class: 'alert') if alert.present?
+  end
+
+  def display_username
+    if current_user.present?
+      current_user.name
+    else
+      'Prijavite se'
+    end
+  end
+
 end
